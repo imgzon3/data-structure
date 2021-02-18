535328fd93fb32d490a1a1ed443c1492a2490fab
imgzon3 <imgzon3@naver.com>
2021-02-18 17:07:14 +0900

    add Print()

--- 1_Linked_LIst/linked_list.py
+++ 1_Linked_LIst/linked_list.py
@@ -15,16 +15,29 @@ class S_linked_list:
             return 0 # 비어있지 않다면 0 return
     
     def List_size(self)-> int: # 리스트의 크기를 반환하는 함수
-        self.list_size = 1
-        self.our_node = None
+        list_size = 1
+        our_node = self.head # head에서 시작
         
-        self.our_node = self.head # head에서 시작
         if self.Empty(): # 비어있다면 0 return
             return 0
         else:
             while True:
-                if self.our_node.next == None: # 다음 node가 없다면
+                if our_node.next == None: # 다음 node가 없다면
                     break
                 else: # 다음 node가 있다면
-                    self.list_size += 1
-                    
+                    list_size += 1
+                    our_node = our_node.next
+            return our_node
+    
+    def Print(self)-> int: # 연결 리스트의 elements를 출력하는 함수
+        our_node = self.head
+        
+        if self.Empty(): # 리스트가 비어있다면 -1 return
+            return -1
+        else:
+            while True:
+                print(our_node.data)
+                if our_node.next == None:
+                    break
+                else:
+                    our_node = our_node.next
