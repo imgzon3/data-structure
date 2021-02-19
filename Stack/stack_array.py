bc483c807743f5ab26dbd7e6653ddc0d90e9ca66
imgzon3 <imgzon3@naver.com>
2021-02-19 19:40:56 +0900

    add top()

--- Stack/stack_array.py
+++ Stack/stack_array.py
@@ -9,4 +9,10 @@ class Stack_array: # 배열을 기반으로 한 스택
     def empty(self)-> bool: # 스택이 비었는지 확인하는 함수
         return len(self.stack) == 0
     
+    def top(self)-> int: # 스택의 맨 위 원소를 확인하는 함수
+        if self.empty(): # 스택이 비어있다면
+            return -1
+        else:
+            return self.stack[self.t]
+    
     
