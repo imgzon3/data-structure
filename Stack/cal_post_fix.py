ac8fe2099d9788cfe6b4cf81fe954c74f2e273e0
imgzon3 <imgzon3@naver.com>
2021-02-20 19:29:35 +0900

    add __init__

--- Stack/cal_post_fix.py
+++ Stack/cal_post_fix.py
@@ -9,17 +9,17 @@ class Stack_array: # 배열을 기반으로 한 스택
     def empty(self)-> bool: # 스택이 비었는지 확인하는 함수
         return len(self.stack) == 0
     
-    def top(self)-> int: # 스택의 맨 위 원소를 확인하는 함수
+    def top(self)-> object: # 스택의 맨 위 원소를 확인하는 함수
         if self.empty(): # 스택이 비어있다면
             return -1
         else:
             return self.stack[self.t] # index t의 값 반환
     
-    def push(self, n: int): # 스택의 새로운 값을 push하는 함수
+    def push(self, n: object): # 스택의 새로운 값을 push하는 함수
         self.stack.append(n) # python은 리스트의 크기 제한이 없으므로, 바로 append
         self.t += 1 # top index + 1 하기
     
-    def pop(self)-> int: # 스택의 top의 값을 삭제하고 반환하는 함수
+    def pop(self)-> object: # 스택의 top의 값을 삭제하고 반환하는 함수
         if self.empty(): # 스택이 비어있다면
             return -1
         else:
@@ -28,9 +28,30 @@ class Stack_array: # 배열을 기반으로 한 스택
 
 class Cal_post_fix:
     def __init__(self, exp: str):
-        self.stack = Stack_array()
-        self.postfix = exp
+        self.stack = Stack_array() # 스택
+        self.postfix = exp # 후위 표기식
         # self.size = len(self.postfix)
-
+        
+        for tmpch in self.postfix:
+            if tmpch!='+' or tmpch!='-' or tmpch!='*' or tmpch!='/': # 피연산자 처리
+                self.stack.push(int(tmpch))
+            else: # 연산자 처리
+                tmp2 = self.stack.pop()
+                tmp1 = self.stack.pop()
+                
+                if tmpch=='+':
+                    tmp2 = tmp1 + tmp2
+                if tmpch=='-':
+                    tmp2 = tmp1 - tmp2
+                if tmpch=='*':
+                    tmp2 = tmp1*tmp2
+                if tmpch=='/':
+                    tmp2 = tmp1/tmp2
+                
+                self.stack.push(tmp2)
+        
+        self.result = self.stack.pop()
+    
+    
 if __name__ == "__main__":
     print(1)
