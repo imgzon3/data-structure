04126d419385ea30091723ba1edb763d0de390e8
imgzon3 <imgzon3@naver.com>
2021-02-21 15:46:07 +0900

    add __init__

--- Linked_LIst/double_linked_list.py
+++ Linked_LIst/double_linked_list.py
@@ -1,6 +1,12 @@
 class Node:
     def __init__(self, e: object):
-        self.element = e
-        self.front = None
-        self.back = None
+        self.element = e # node가 저장하는 element
+        self.front = None # 앞에 있는 node의 주소 저장
+        self.back = None # 뒤에 있는 node의 주소 저장
 
+class D_linked_list:
+    def __init__(self):
+        self.head = None # head에 위치하는 node의 주소 값 저장
+        self.tail = None # tail에 위치하는 node의 주소 값 저장
+    
+    
