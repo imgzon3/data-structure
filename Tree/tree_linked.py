9d579c15bd76cfcba9227c7d07c885511fe951de
imgzon3 <imgzon3@naver.com>
2021-02-23 19:59:43 +0900

    add set_parent()

--- Tree/tree_linked.py
+++ Tree/tree_linked.py
@@ -18,10 +18,13 @@ class Node:
         self.parent = None
         self.child = []
     
-    def insert_child(self, e:Node): # 해당 node에 자식을 삽입할 때 사용하는 함수
-        self.child.append(e)
+    def insert_child(self, n:Node): # 해당 node에 자식을 삽입할 때 사용하는 함수
+        self.child.append(n)
     
-    def del_child(self, e:Node): # 현재 node의 자식 node 중 특정 node를 제거
+    def del_child(self, n:Node): # 현재 node의 자식 node 중 특정 node를 제거
         for i in self.child:
-            if i==e:
-                self.child.pop(i)
+            if i==n:
+                self.child.remove(i)
+    
+    def set_parent(self, n:Node): # 현재 node의 부모를 바꾸는 함수
+        self.parent = n
