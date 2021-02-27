6c556f720d47f38619e12fb2d76d36a7decaeb34
imgzon3 <imgzon3@naver.com>
2021-02-27 14:05:10 +0900

    주석 변경

--- Heap/array_base_heap.py
+++ Heap/array_base_heap.py
@@ -4,6 +4,7 @@ heap 필요한 것
 2. min-heap, max-heap 구별(+1, -1입력받고, 곱해서 구별하기)
 
 heap functions
+0. is_empty()
 1. swap()
 2. upheap()
 3. downheap()
@@ -12,10 +13,12 @@ heap functions
 6. top()
 7. size()
 8. print()
-9. is_empty()
-10. find()
+9. find()
 '''
 class Heap:
     def __init__(self, dir: int): # 1 입력받으면 min-heap, -1 입력받으면 max-heap
         self.array = [-1] # 0번째 index값은 사용 안함
         self.dir = dir
+    
+    def swap(self, idx1: int, idx2: int): # 입력받은 2개의 inx에 저장되어 있는 값을 서로 바꿈
+        
