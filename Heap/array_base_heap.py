a599b8c68a3ef7065986eee7f384d9b3a12d6469
imgzon3 <imgzon3@naver.com>
2021-02-27 13:57:00 +0900

    주석 추가

---
+++ Heap/array_base_heap.py
@@ -0,0 +1,19 @@
+'''
+heap 필요한 것
+1. 저장할 배열
+2. min-heap, max-heap 구별(+1, -1입력받고, 곱해서 구별하기)
+
+heap functions
+1. swap()
+2. upheap()
+3. downheap()
+4. insert()
+5. pop()
+6. top()
+7. size()
+8. print()
+9. is_empty()
+10. find()
+'''
+class Heap:
+    
