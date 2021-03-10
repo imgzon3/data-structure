3448a210dbdd8c876c18fc66df911e0ab1fe8272
imgzon3 <imgzon3@naver.com>
2021-03-10 20:29:32 +0900

    add put()

--- Hash/linear_hash_table.py
+++ Hash/linear_hash_table.py
@@ -1,3 +1,22 @@
+'''
+linear probing hashing
+필요 함수
+1. put
+2. del
+3. show
+4. find
+'''
 class Hashtable:
-    def __init__(self, n: int):
-        self.table = [None for _ in range(n)]
+    def __init__(self):
+        self.table = [None for _ in range(13)]
+    
+    def put(self, key: int, value: str):
+        while True:
+            if not(self.table[key%13]):
+                self.table[key%13] = value
+            else:
+                key += 1
+
+if __name__ == '__main__':
+    hashing = Hashtable()
+    
