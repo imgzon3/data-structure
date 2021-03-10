9d010bacd2da8f0445882a05ff651a82a8d3244d
imgzon3 <imgzon3@naver.com>
2021-03-10 20:39:50 +0900

    add del()

--- Hash/linear_hash_table.py
+++ Hash/linear_hash_table.py
@@ -10,12 +10,35 @@ class Hashtable:
     def __init__(self):
         self.table = [None for _ in range(13)]
     
+    def full(self)-> bool:
+        tmp = True
+        for i in self.table:
+            if not(i):
+                tmp = False
+                break
+        return tmp
+    
     def put(self, key: int, value: str):
+        if not(self.full()):
+            while True:
+                if not(self.table[key%13]):
+                    self.table[key%13] = value
+                else:
+                    key += 1
+    
+    def del(self, key: int)-> str:
         while True:
-            if not(self.table[key%13]):
-                self.table[key%13] = value
+            if self.table[key%13]:
+                print(f'There is no value in {key}')
+                break
             else:
-                key += 1
+                if self.table[key%13] == False:
+                    key += 1
+                else:
+                    tmp = self.table[key%13]
+                    self.table[key%13] = False
+                    return tmp
+
 
 if __name__ == '__main__':
     hashing = Hashtable()
