a3592830cf4f940036b92006837b50219990fea1
imgzon3 <imgzon3@naver.com>
2021-03-26 12:24:43 +0900

    change def insert()

--- Tree/binary_search_tree.py
+++ Tree/binary_search_tree.py
@@ -18,7 +18,8 @@ class SearchTree:
     def insert(self, nd, i:int): # insert, root 비었으면 root에 삽입, 아닐 시 재귀
         curNode = nd
         if self.root == None: # root가 비어있을 경우
-            self.root = tmpNode
+            tmp = Node(i)
+            self.root = tmp
         else:
             if curNode.data <= i: # data가 현재 노드의 데이터 보다 크면 오른쪽 node로
                 if curNode.right == None:
@@ -26,14 +27,14 @@ class SearchTree:
                     curNode.right = tmp
                     tmp.parent = curNode
                 else:
-                    insert(curNode.right, i)
+                    self.insert(curNode.right, i)
             elif curNode.data > i: # data가 현재 노드의 데이터보다 작으면 왼쪽 node로
                 if curNode.left == None:
                     tmp = Node(i)
                     curNode.left = tmp
                     tmp.parent = curNode
                 else:
-                    insert(curNode.left, i)
+                    self.insert(curNode.left, i)
 
 if __name__ == "__main__":
     searchTree = SearchTree()
