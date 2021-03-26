eec6d75f2f58d71344e8f5805bbd408cfcced518
imgzon3 <imgzon3@naver.com>
2021-03-26 13:04:35 +0900

    change test.py

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-a = [None for _ in range(10)]
-
-if not(a[5]):
-  print('asdf')
+class Node:
+    def __init__(self, i: int):
+        self.data = i
+        self.left = self.right = None
+        self.parent = None
+        
+if __name__ == "__main__":
+    root = Node(1)
+    tmp = Node(2)
+    root.right = tmp
+    print(type(tmp.right))
