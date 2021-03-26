225c1f5177c41dbd45e7408da631665cbb41d07c
imgzon3 <imgzon3@naver.com>
2021-03-26 12:24:01 +0900

    add def insert()

--- Tree/binary_search_tree.py
+++ Tree/binary_search_tree.py
@@ -7,12 +7,33 @@ binary search tree
 '''
 class Node:
     def __init__(self, i: int):
-        self.data = i
-        self.left = self.right = None
+        self.data = i # data 값
+        self.left = self.right = None # 두 자식 주소
+        self.parent = None # 부모 주소
 
 class SearchTree:
-    def __init__(self, i: int):
-        self.root = Node(i)
+    def __init__(self):
+        self.root = None
+
+    def insert(self, nd, i:int): # insert, root 비었으면 root에 삽입, 아닐 시 재귀
+        curNode = nd
+        if self.root == None: # root가 비어있을 경우
+            self.root = tmpNode
+        else:
+            if curNode.data <= i: # data가 현재 노드의 데이터 보다 크면 오른쪽 node로
+                if curNode.right == None:
+                    tmp = Node(i)
+                    curNode.right = tmp
+                    tmp.parent = curNode
+                else:
+                    insert(curNode.right, i)
+            elif curNode.data > i: # data가 현재 노드의 데이터보다 작으면 왼쪽 node로
+                if curNode.left == None:
+                    tmp = Node(i)
+                    curNode.left = tmp
+                    tmp.parent = curNode
+                else:
+                    insert(curNode.left, i)
 
 if __name__ == "__main__":
-    
+    searchTree = SearchTree()
